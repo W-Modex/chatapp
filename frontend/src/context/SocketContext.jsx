55492c228a416a2fb8dba306efa45129123009a3
Modex <rifai.ali259@gmail.com>
2024-03-23 00:12:49 +0000

    add socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chatapp-p1z8.onrender.com", {
                 query: {
                     userId: authUser._id
                 }
